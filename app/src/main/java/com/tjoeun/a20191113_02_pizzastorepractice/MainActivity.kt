dc994891dc104d7b3f1b7f87b0c10baae8095afd
kj_cho <kj_cho@idealidea.co.kr>
2019-11-13 21:40:32 +0900

    리스트 아이템까지 작성

--- app/src/main/java/com/tjoeun/a20191113_02_pizzastorepractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191113_02_pizzastorepractice/MainActivity.kt
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tjoeun.a20191113_02_pizzastorepractice.datas.PizzaStoreData
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : BaseActivity() {
     var pizzaStoreList = ArrayList<PizzaStoreData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
         addPizzaStores()
     }
+
+
 
     fun addPizzaStores() {
         pizzaStoreList.add(PizzaStoreData("피자헛", "010-5134-9754", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
