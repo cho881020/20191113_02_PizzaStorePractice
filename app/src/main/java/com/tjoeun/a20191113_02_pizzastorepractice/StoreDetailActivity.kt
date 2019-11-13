4ee31039d9e15fcec1e23c0bc0bb7ffad954948d
kj_cho <kj_cho@idealidea.co.kr>
2019-11-13 22:30:49 +0900

    전화 걸기 기능까지

--- app/src/main/java/com/tjoeun/a20191113_02_pizzastorepractice/StoreDetailActivity.kt
+++ app/src/main/java/com/tjoeun/a20191113_02_pizzastorepractice/StoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package com.tjoeun.a20191113_02_pizzastorepractice
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.tjoeun.a20191113_02_pizzastorepractice.datas.PizzaStoreData
 import kotlinx.android.synthetic.main.activity_store_detail.*
 
@@ -18,6 +24,30 @@ class StoreDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+            var permissionListener : PermissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    var uri = Uri.parse("tel:${storeData?.phoneNum?.replace("-", "")}")
+                    var intent = Intent(Intent.ACTION_CALL, uri)
+                    startActivity(intent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한을 설정해야 전화 사용이 가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("전화 권한을 설정해야 바로 연결 가능합니다.")
+                .setDeniedMessage("설정 > 권한에서 허용 가능합니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
+
     }
 
     override fun setValues() {
